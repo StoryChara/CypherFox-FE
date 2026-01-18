8aaadb837ddaa67a6d290cdc5fa8de1c45432182
StoryChara <mjarah@unal.edu.co>
2026-01-18 15:08:22 -0500

    :chart_with_upwards_trend: Añadir Vercel Spped Insights

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,11 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>,
 );
